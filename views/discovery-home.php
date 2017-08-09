dbf182afb448a7281cde2c0393c02a673211f6e8
Eugene Siow <kyo116@gmail.com>
2017-08-09 14:08:40 +0100

    Initial Commit

--- views/discovery-home.php
+++
@@ -1,6 +0,0 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.5/d3.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.3/moment.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.6.5/js/bootstrap-select.min.js"></script>
-<script src="/js/bootstrap-datepicker.min.js"></script>
-<script src="/js/nv.d3.min.js"></script>
-<script src="/js/common.js"></script>
--- views/home.php
+++
@@ -1,8 +0,0 @@
-<div class="container-fluid">
-
-    <div class="frontpage-info">
-        <h1><span class="icon-pismarthome"></span> PiSmartHome</h1>
-        <p id="content" class="lead">A LinkedData Store and Analytics Engine<br/> on a <span class="highlight">RaspberryPi</span> for Smart Homes.</p>
-    </div>
-
-</div><!-- /.container -->
